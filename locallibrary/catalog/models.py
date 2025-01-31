7a39b620bc5233a5c3628db2efe618c7a56716da
ana03lm <ana.lopez.morales2025@gmail.com>
2025-01-31 17:45:00 +0100

    Tutoriales hasta la mitad del 8

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
-
 import uuid # Required for unique book instances
+from django.conf import settings
+from datetime import date
+
 
 # Create your models here.
 
@@ -33,6 +34,7 @@ class Genre(models.Model):
             ),
         ]
         
+        
 class Language(models.Model):
     """Model representing the language of the book."""
     name = models.CharField(
@@ -56,6 +58,7 @@ class Language(models.Model):
                 violation_error_message = "Language already exists (case insensitive match)"
             ),
         ]
+        
         
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -89,6 +92,7 @@ class Book(models.Model):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
 
+
 class BookInstance(models.Model):
 
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -97,6 +101,8 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -113,12 +119,18 @@ class BookInstance(models.Model):
         help_text='Book availability',
     )
 
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
     class Meta:
         ordering = ['due_back']
 
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
 
 class Author(models.Model):
     """Model representing an author."""
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 
@@ -17,12 +18,39 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_fantasia = Book.objects.filter(genre__name__iexact='fantasia').count()
+    num_ejemplo_books = Book.objects.filter(title__contains='ejemplo').count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_fantasia': num_fantasia,
+        'num_ejemplo_books': num_ejemplo_books,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    
+class BookDetailView(generic.DetailView):
+    model = Book
+    
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
